660d22d50ea1736d4edc4ead905dc81422082c6f
Marco Vavassori <marcovava2001@gmail.com>
2025-02-03 19:22:43 +0000

    Empty image component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageComponent } from './image/image.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
